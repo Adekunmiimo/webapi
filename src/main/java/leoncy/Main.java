7027bdf2773d06154c13ba6bf84665d0eabcea81
Adekunmi <adekunmiimooluwa@gmail.com>
2023-07-28 16:04:55 +0100

    first commit

---
+++ src/main/java/leoncy/Main.java
@@ -0,0 +1,38 @@
+package leoncy;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response =new GreetResponse(
+            "Hello", 
+            List.of("Java", "Golang", "Javascript"),
+            new Person("Adekunmi Imooluwa Adekeye", 28, 30_000)
+         
+        );
+        return response;
+    }
+
+  
+    record Person(String name, int age, double savings){
+
+    }
+    record GreetResponse(
+        String greet, 
+        List<String> favProgrammingLanguages,
+        Person person
+        ){
+
+    }
+}
